3a2cb83ba40ffa03c9aa1b61633bdda55b372e21
Brandon Carlos <brandon_marzo@hotmail.com>
2024-08-13 18:19:24 -0300

    Adding variables of environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+    XRapidAPIHostLabel: 'open-weather-map27.p.rapidapi.com',
+};
